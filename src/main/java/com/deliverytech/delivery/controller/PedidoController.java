259027060e661e58320e3d032ddc1e9f54af1a80
Luiz Henrique <unnathls.dev@gmail.com>
2025-08-21 00:37:04 -0300

    Feat: Implementado Criação de pedido , lista de pedidos e atualização de pedido

--- src/main/java/com/deliverytech/delivery/controller/PedidoController.java
+++ src/main/java/com/deliverytech/delivery/controller/PedidoController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.service.annotation.PatchExchange;
 
 import com.deliverytech.delivery.dto.PedidoRequestDTO;
 import com.deliverytech.delivery.dto.PedidoResponseDTO;
@@ -32,9 +36,43 @@ public class PedidoController {
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
-    @GetMapping
-    public ResponseEntity<List<PedidoResponseDTO>> listarPedido(){
+    @GetMapping("/{id}")
+    public ResponseEntity<List<PedidoResponseDTO>> listarPedido(@PathVariable Long id) {
         List<PedidoResponseDTO> listaDePedidoDTO = pedidoService.listarPedidos();
         return new ResponseEntity<>(listaDePedidoDTO, HttpStatus.OK);
+    }
+    @PutMapping("/{id}/confirmar")
+    public ResponseEntity<PedidoResponseDTO>confirmaPedido(@PathVariable Long id){
+        Pedido pedidoConfirmado = pedidoService.confirmarPedido(id);
+        PedidoResponseDTO responseDTO = pedidoService.toResponseDTO(pedidoConfirmado);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+    
+    @PutMapping("/{id}/preparo")
+    public ResponseEntity<PedidoResponseDTO> prepararPedido(@PathVariable Long id) {
+        Pedido pedidoPreparado = pedidoService.iniciarPreparo(id);
+        PedidoResponseDTO responseDTO = pedidoService.toResponseDTO(pedidoPreparado);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/enviar")
+    public ResponseEntity<PedidoResponseDTO> enviarPedido(@PathVariable Long id){
+        Pedido pedidoEnviado = pedidoService.enviarPedido(id);
+        PedidoResponseDTO responseDTO = pedidoService.toResponseDTO(pedidoEnviado);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/entregar")
+    public ResponseEntity<PedidoResponseDTO> entregarPedido(@PathVariable Long id){
+        Pedido pedidoEntregue = pedidoService.entregarPedido(id);
+        PedidoResponseDTO responseDTO = pedidoService.toResponseDTO(pedidoEntregue);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/cancelar")
+    public ResponseEntity<PedidoResponseDTO> cancelarPedido(@PathVariable Long id){
+        Pedido pedidoCancelado = pedidoService.cancelarPedido(id);
+        PedidoResponseDTO responseDTO = pedidoService.toResponseDTO(pedidoCancelado);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 }
